9681e0a48ccdecb73859d683171354bc9bc3a912
Jeo <jeo961@gmail.com>
2021-04-12 14:45:08 -0400

    first commmit

---
+++ smallclass/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace smallclass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] names = { "Sean", "Kalai", "Jean", "Justin", "Alice", "Jason", "Kristen"};
+            string[] cities = { "Plymouth", "Troy", "Lascahobas", "Wyoming", "Detroit", "Courtright", "Orange Park"};
+
+            for(int i = 0; i < names.Length; i++)
+            {
+                Console.WriteLine($"Name: {names[i]}\tCity: {cities[i]}");
+            }
+        }
+    }
+}
